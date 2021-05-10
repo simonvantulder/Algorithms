5d7dde45b070421b239eb1b60e93e8a6e4281200
simonvantulder <vantulder.simon@gmail.com>
2021-05-10 13:02:37 -0700

    orderedIntersection

---
+++ orderedIntersection.js
@@ -0,0 +1,42 @@
+/* 
+  Efficiently combine two already sorted multiset arrays 
+  into an array containing the sorted set intersection of the two.
+  Output: only the shared values between the two arrays (deduped).
+  Venn Diagram Visualization (bottom) https://i.ytimg.com/vi/sdflTUW6gHo/maxresdefault.jpg
+*/
+
+const numsA1 = [0, 1, 2, 2, 2, 7];
+const numsB1 = [2, 2, 6, 6, 7];
+const expected1 = [2, 7];
+
+const numsA2 = [0, 1, 2, 2, 2, 7];
+const numsB2 = [2, 2];
+const expected2 = [2];
+
+/**
+ * Venn Diagram Visualization (bottom):
+ * https://i.ytimg.com/vi/sdflTUW6gHo/maxresdefault.jpg
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<number>} sortedA
+ * @param {Array<number>} sortedB Both sets are multisets
+ *    (multi in that it can contain multiple dupes).
+ * @returns {Array<number>} The sorted set intersection: a new array that is
+ *    sorted and contains only the shared values between the two arrays
+ *    deduped.
+ */
+function orderedIntersection(sortedA, sortedB) {
+    setA = new Set(sortedA);
+    newArr = [];
+    freq = {};
+    for(var i = 0; i < sortedB.length; i++){
+        if(setA.has(sortedB[i]) && !freq.hasOwnProperty(sortedB[i]) ){ //&& freq !has sorted[i] 
+            //add to freq
+            freq[sortedB[i]]++;
+            newArr.push(sortedB[i]);
+        }
+    }
+    return newArr;
+}
+
+console.log(orderedIntersection(numsA1, numsB1));
