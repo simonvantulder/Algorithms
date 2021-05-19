04b717a4e353321b057c55cea771096724d88c7e
simonvantulder <vantulder.simon@gmail.com>
2021-05-19 10:51:30 -0700

    Co-authored-by: Peter  <phellwege@users.noreply.github.com>

---
+++ keyFreq.js
@@ -0,0 +1,118 @@
+/* 
+    Given an array of objects, a searchFor string, and searchBy key that exists in the object
+    return a new array of only those objects whose value for the given key starts with the given search string
+    You can assume the key will exist on the object and the value of that key will be a string
+    Bonus: make the search case insensitive
+    Bonus: re-write it with functional programming in mind, using built in methods
+    Bonus: allow the search method to be provided as a parameter, e.g., string methods: includes, startsWith, endsWith
+    - you can assume the searchMethod will be valid
+*/
+
+const people = [
+    {
+        firstName: "John",
+        lastName: "Doe",
+    },
+    {
+        firstName: "Jane",
+        lastName: "Doe",
+    },
+    {
+        firstName: "Eddy",
+        lastName: "Lee",
+    },
+    {
+        firstName: "John",
+        lastName: "Fawn",
+    },
+    {
+        firstName: "Edward",
+        lastName: "Kim",
+    },
+];
+
+const searchFor1 = "Jo";
+const searchBy1 = "firstName";
+const expected1 = [
+    {
+        firstName: "John",
+        lastName: "Doe",
+    },
+    {
+        firstName: "John",
+        lastName: "Fawn",
+    },
+];
+
+const searchFor2 = "ohn";
+const searchBy2 = "firstName";
+const expected2 = [];
+// Explanation: "John" contains "ohn", it does not start with "ohn"
+
+const searchFor3 = "Do";
+const searchBy3 = "lastName";
+const expected3 = [
+    {
+        firstName: "John",
+        lastName: "Doe",
+    },
+    {
+        firstName: "Jane",
+        lastName: "Doe",
+    },
+];
+
+// Bonus
+const searchFor4 = "E";
+const searchBy4 = "lastName";
+const searchMethod4 = "includes";
+const expected4 = [
+    {
+        firstName: "John",
+        lastName: "Doe",
+    },
+    {
+        firstName: "Jane",
+        lastName: "Doe",
+    },
+    {
+        firstName: "Eddy",
+        lastName: "Lee",
+    },
+];
+
+/**
+ * Filters the given items based on the search criteria using a startsWith
+ * search method.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<Object>} items The items to be filtered.
+ * @param {string} searchBy The key to search by.
+ * @param {string} searchFor The value of the given key to search for.
+ * @returns {Array<Objects>} The matched items.
+ */
+function filterByKey(items, searchFor, searchBy, searchMethod = "startsWith") {
+    // items.filter(searchFor);
+    let matchedSearches = [];
+    for (const personObject of items) {
+        // console.log(personObject)
+        // console["log"](personObject[searchBy])
+        if (personObject[searchBy].toLowerCase()[searchMethod](searchFor.toLowerCase())) {
+            matchedSearches.push(personObject);
+        }
+    }
+    return matchedSearches;
+}
+
+//oneLine Solution 
+function filterByKey(items, searchFor, searchBy, stringSearchMethod = "startsWith") {
+    return items.filter((item) => item[searchBy].toLowerCase()[stringSearchMethod](searchFor.toLowerCase()));
+}
+
+console.log(filterByKey(people, searchFor1, searchBy1))
+console.log("*************")
+console.log(filterByKey(people, searchFor2, searchBy2))
+console.log("*************")
+console.log(filterByKey(people, searchFor3, searchBy3))
+console.log("*************")
+console.log(filterByKey(people, searchFor4, searchBy4, searchMethod4))
