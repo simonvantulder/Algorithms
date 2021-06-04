6a108dfaaace10e2e7c811e548405defde9b0198
simonvantulder <vantulder.simon@gmail.com>
2021-06-04 11:16:49 -0700

    learning Binary

---
+++ convertFromBinary.js
@@ -0,0 +1,8 @@
+const binaryArrayToNumber = arr => {
+    let binaryStr = ""
+    arr.map((i) => binaryStr += i);
+    return parseInt(binaryStr, 2)
+};
+
+//refactored
+const binaryArrayToNumber2 = arr => parseInt(arr.join(''), 2);
