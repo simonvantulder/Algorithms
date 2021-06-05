dd5c815c1012e6b3ae33f4be5c053aafb31bd5ca
simonvantulder <vantulder.simon@gmail.com>
2021-06-05 12:20:41 -0700

    sort zeros from arr

---
+++ move_zeros.js
@@ -0,0 +1,19 @@
+
+let moveZeros = function (arr) {
+    for (var i = arr.length - 1; i >= 0; i--) {
+        if (arr[i] === 0) {
+            arr.splice(i, 1); //remove 0 from arr & shift values
+            arr.push(0); //add to the end
+        }
+    }
+    return arr;
+}
+
+//refactored
+let moveZeros2 = function (arr) {
+    return [
+        ...(arr.filter(n => n !== 0)),
+        ...(arr.filter(n => n === 0))
+    ];
+}
+console.log(moveZeros([1, 2, 0, 1, 0, 1, 0, 3, 0, 1]))
