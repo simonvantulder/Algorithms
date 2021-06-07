1c89d11afd4a0d444db97a36968f0005896c35fd
simonvantulder <vantulder.simon@gmail.com>
2021-06-07 11:04:28 -0700

    create phone number algo

---
+++ createPhoneNumber.js
@@ -0,0 +1,43 @@
+//straightforward oneliner soln
+function createPhoneNumber(arr){
+    return '('+arr[0]+arr[1]+arr[2]+') '+arr[3]+arr[4]+arr[5]+'-'+arr[6]+arr[7]+arr[8]+arr[9];
+}
+
+//standard soln
+function createPhoneNumber(arr) {
+    if (arr.length !== 10) {
+        return arr;
+    }
+    let phoneStr = "("
+    for (let i = 0; i < arr.length; i++) {
+        if (isNaN(arr[i])) {
+            return arr;
+        }
+        if (i === 3) {
+            phoneStr += ") ";
+        }
+        else if (i === 6) {
+            phoneStr += "-";
+        }
+        phoneStr += arr[i];
+    }
+    return phoneStr;
+}
+
+//refactored
+function createPhoneNum(arr) {
+    arr = arr.join('');
+    return '(' + arr.substring(0, 3) + ') ' + arr.substring(3, 6) + '-' + arr.substring(6);
+}
+
+// alt Codewars solution
+function createPhoneNumber(numbers) {
+    var format = "(xxx) xxx-xxxx";
+
+    for (var i = 0; i < numbers.length; i++) {
+        format = format.replace('x', numbers[i]);
+    }
+
+    return format;
+}
+console.log(createPhoneNum([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
--- move_zeros.js
+++ move_zeros.js
@@ -1,4 +1,3 @@
-
 let moveZeros = function (arr) {
     for (var i = arr.length - 1; i >= 0; i--) {
         if (arr[i] === 0) {
