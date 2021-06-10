7a6b8f519332347b85615d05e00e5d9f99e15bc8
simonvantulder <vantulder.simon@gmail.com>
2021-06-10 09:06:22 -0700

    playing with digits & exponents

--- playingWithDigits.js
+++ playingWithDigits.js
@@ -1,15 +1,17 @@
 function digPow(n, p) {
-    nStr = n.toString()
-    sum = 0
+    nStr = n.toString() // cast int as string to loop through
+    sum = 0;
+
+    //loop through each number and raise it to power of p+i
     for (let i = 0; i < nStr.length; i++) {
         sum += parseInt(nStr[i]) ** (p + i);
     }
-    return sum % n === 0 ? sum / n : -1;
+    return sum % n === 0 ? sum / n : -1; //return solution if sum of powers is divisible by n else -1
 }
 
 //refactored
 function digPow2(n, p) {
-    // "**" is the exp shorthand in js or use Math.pow()
+    // "**" is the exp shorthand in js, can also use Math.pow()
     var x = String(n).split("").reduce((sum, digit, i) => sum += (digit ** (p + i)), 0) 
     return x % n ? -1 : x / n;
 }
