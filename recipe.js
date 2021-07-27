356b7f70c6ce14c0f804535361fff29650f466c9
simonvantulder <vantulder.simon@gmail.com>
2021-07-27 13:13:51 -0700

    recipe batches

---
+++ recipe.js
@@ -0,0 +1,26 @@
+function cakes(recipe, available) {
+
+    // if(recipe.keys().length === 0 || available.keys().length === 0){
+    //     return 0;
+    // }
+    let recipeBatches = Infinity;
+    for (let i in recipe) {
+        if(available.hasOwnProperty(i)){
+            if (available[i] < recipe[i]){
+                return 0
+            } else { //have enough
+                if ((available[i] / recipe[i]) < recipeBatches){
+                    recipeBatches = Math.floor(available[i] / recipe[i]);
+                }
+            }
+        } else{
+            return 0
+        }
+    }
+    return recipeBatches
+}
+
+// must return 2
+console.log( cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200})); 
+// must return 0
+// console.log( cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000})); 
--- stayPositive.js
+++ stayPositive.js
@@ -1,6 +1,6 @@
 function minStart(arr) {
     //need a running total
-    /* need a starting value, start with min number neccessary to keep the first num positive. 
+    /* need a starting value, start with min number neccessary to keep the starting num positive. 
      * increment from there if necessary
     */
     if(arr.length === 0){
@@ -9,24 +9,24 @@ function minStart(arr) {
     let minStartVal = 1 - arr[0];
     let runningTotal = minStartVal;
     
-    if(1-arr[0] < 1){
-        runningTotal = minStartVal = 9
-    }
-    console.log("minStartVal =", minStartVal);
+
+    // console.log("minStartVal =", minStartVal);
+    // console.log("enter loop");
 
     for(let i = 0 ; i < arr.length; i++){
         runningTotal+= arr[i]
-        console.log(arr[i], runningTotal);
+        // console.log(arr[i], runningTotal);
         if(runningTotal < 1) {
             console.log("runningtotal = ", runningTotal);
             let oldStartVal = minStartVal;
             minStartVal += 1 - runningTotal; //running total is already negative-> two negs = positive
             runningTotal += minStartVal - oldStartVal ;
+            console.log("runningtotal now = ", runningTotal, "minstart = ", minStartVal);
 
         }  
     }
-    console.log("minstart = ", minStartVal);
+    console.log("minstart = ", minStartVal, "runningtotal = ", runningTotal);
     return minStartVal
 }
 
-console.log(minStart([10,-5,4,-2,3,1,-1,-6,-1,0,8]));
+minStart([10,-5,4,-2,3,1,-1,-6,-1,0,8]);
