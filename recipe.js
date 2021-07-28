7d38c6df82f7c0cad1448107c574c87d7df37d93
simonvantulder <vantulder.simon@gmail.com>
2021-07-28 10:12:36 -0700

    alt soln

--- recipe.js
+++ recipe.js
@@ -1,26 +1,50 @@
 function cakes(recipe, available) {
-
-    if(Object.keys(recipe).length === 0 || Object.keys(recipe).length === 0){
+    if (Object.keys(recipe).length === 0 || Object.keys(recipe).length === 0) {
         return 0;
     }
     let recipeBatches = Infinity;
     for (let i in recipe) {
-        if(available.hasOwnProperty(i)){
-            if (available[i] < recipe[i]){
-                return 0
-            } else { //have enough
-                if ((available[i] / recipe[i]) < recipeBatches){
+        if (available.hasOwnProperty(i)) {
+            if (available[i] < recipe[i]) {
+                return 0;
+            } else {
+                //have enough
+                if (available[i] / recipe[i] < recipeBatches) {
                     recipeBatches = Math.floor(available[i] / recipe[i]);
                 }
             }
-        } else{
-            return 0
+        } else {
+            return 0;
         }
     }
-    return recipeBatches
+    return recipeBatches;
 }
 
 // must return 2
-console.log( cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200})); 
+console.log(
+    cakes(
+        { flour: 500, sugar: 200, eggs: 1 },
+        { flour: 1200, sugar: 1200, eggs: 5, milk: 200 }
+    )
+);
 // must return 0
-// console.log( cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000})); 
+// console.log( cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000}));
+
+// =============== solutionn 2 from codewars =================
+
+function cakes(recipe, available) {
+    var numCakes = [];
+
+    for (var key in recipe) {
+        if (recipe.hasOwnProperty(key)) {
+            if (key in available) {
+                numCakes.push(Math.floor(available[key] / recipe[key]));
+            } else {
+                return 0;
+            }
+        }
+    }
+
+    // i liked this even if it is another loop
+    return Math.min.apply(null, numCakes);
+}
